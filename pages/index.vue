bcb33961a740d4445a75078ac6b1a9c7466c8fa9
Arek <arkadiuszp@arcor.de>
2023-05-17 15:34:23 +0200

    update page meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,50 @@
 <script setup lang="ts">
 useHead({
   title: "Fritscher - Frontend developer portfolio",
-  meta: [{ name: "description", content: "Entwickler Portfolio von Arkadiusz Fritscher" }],
+  meta: [
+    { name: "description", content: "Entwickler Portfolio von Arkadiusz Fritscher" },
+    { name: "msapplication-TileColor", content: "#20163e" },
+    { name: "theme-color", content: "#20163E" },
+  ],
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest",
+    },
+    {
+      rel: "mask-icon",
+      href: "/safari-pinned-tab.svg",
+      color: "#623fc9",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+
+    // {
+    //   rel: "icon",
+    //   type: "image/svg+xml",
+    //   href: "/favicon.svg",
+    // },
+    // {
+    //   rel: "alternate icon",
+    //   type: "image/png",
+    //   href: "/favicon.png",
+    // },
+  ],
   // bodyAttrs: {
   //   class: "test",
   // },
