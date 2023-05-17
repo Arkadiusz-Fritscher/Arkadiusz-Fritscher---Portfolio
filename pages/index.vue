b8c9ee3c029021cfd2d15dbfe12e8ca116e348cc
Arek <arkadiuszp@arcor.de>
2023-05-17 15:15:25 +0200

    change page meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 useHead({
   title: "Fritscher - Frontend developer portfolio",
-  meta: [{ name: "description", content: "My amazing site." }],
-  bodyAttrs: {
-    class: "test",
-  },
-  script: [{ innerHTML: "console.log('Hello world')" }],
+  meta: [{ name: "description", content: "Entwickler Portfolio von Arkadiusz Fritscher" }],
+  // bodyAttrs: {
+  //   class: "test",
+  // },
+  // script: [{ innerHTML: "console.log('Hello world')" }],
 });
 
 useServerSeoMeta({
-  title: "My Amazing Site",
-  ogTitle: "My Amazing Site",
-  description: "This is my amazing site, let me tell you all about it.",
-  ogDescription: "This is my amazing site, let me tell you all about it.",
-  ogImage: "https://example.com/image.png",
+  title: "Fritscher - Frontend developer portfolio",
+  ogTitle: "Fritscher - Frontend developer portfolio",
+  description: "Entwickler Portfolio von Arkadiusz Fritscher",
+  ogDescription: "Entwickler Portfolio von Arkadiusz Fritscher",
+  ogImage: "https://arkadiusz-fritscher-portfolio.vercel.app/oGImage.jpg",
   twitterCard: "summary_large_image",
 });
 </script>
