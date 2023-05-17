8a1f9f760345ce8cea04a72d4f429f0d472685b3
Arek <arkadiuszp@arcor.de>
2023-05-17 20:55:11 +0200

    settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,27 +3,6 @@ export default defineNuxtConfig({
   ssr: true,
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   modules: ["@nuxt/image-edge"],
-  image: {
-    presets: {
-      project: {
-        modifiers: {
-          format: "webp",
-          width: 1330,
-          height: 1000,
-        },
-      },
-    },
-    // The screen sizes predefined by `@nuxt/image`:
-    screens: {
-      xs: 320,
-      sm: 640,
-      md: 768,
-      lg: 1024,
-      xl: 1280,
-      xxl: 1536,
-      "2xl": 1536,
-    },
-  },
   css: ["@/assets/css/reset.css", "@/assets/css/global.css", "@/assets/css/lensi.css"],
   app: {
     head: {
